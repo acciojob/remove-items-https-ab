e9cd5c0660801492f1c2344308716dc4be9a1177
Anushka A. Bhasme <78693700+https-ab@users.noreply.github.com>
2025-09-26 11:10:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function removeColor {
+	let select = document.getElementById("colorSelect");
+
+	let selectedColor = select.value;
+
+	for (let i = 0; i < select.options.length; i++) {
+		if(select.options.value[i] === selectedColor) {
+			select.remove(i);
+			break;
+		}
+	}
